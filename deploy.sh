60b8858c35fea315a3847650152a4b99320c9d6a
Ayush Priya <ayushpriya10@gmail.com>
2020-01-13 11:58:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 cd dvna && git pull origin master
 export MYSQL_USER=root
 export MYSQL_DATABASE=dvna
-export MYSQL_PASSWORD=AyushPriya#10
+export MYSQL_PASSWORD=<db password>
 export MYSQL_HOST=127.0.0.1
 export MYSQL_PORT=3306
 pm2 start server.js
