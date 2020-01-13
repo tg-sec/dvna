a9e7cbe10e6be7d8aa6db1effa04e8b109965ee9
Ayush Priya <ayushpriya10@gmail.com>
2020-01-13 12:00:37 +0530

    Replacing 127.0.0.1 to localhost

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ cd dvna && git pull origin master
 export MYSQL_USER=root
 export MYSQL_DATABASE=dvna
 export MYSQL_PASSWORD=<db password>
-export MYSQL_HOST=127.0.0.1
+export MYSQL_HOST=localhost
 export MYSQL_PORT=3306
 pm2 start server.js
