73c9e20d1fa97d160bae08195bc81a1c19cf061a
tg-sec <tgsec>
2022-07-05 13:01:33 +0530

    Update Jenkinsfile

--- scripts/nodejsscan.sh
+++ scripts/nodejsscan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SAST_REPORTS="/var/lib/jenkins/reports"
-nodejsscan --directory `pwd` --output ${SAST_REPORTS}/nodejsscan-report'
+nodejsscan --directory `pwd` --output ${SAST_REPORTS}/nodejsscan-report
