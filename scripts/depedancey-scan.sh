ef480fb21bd6bf49b5d7e5bfaef8ef3b8bab1062
tg-sec <tgsec>
2022-07-06 12:35:06 +0530

    Update Scripts

--- scripts/depedancey-scan.sh
+++ scripts/depedancey-scan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SAST_REPORT="/var/lib/jenkins/reports"
-dependency-check.sh --scan `pwd` -f JSON -o $SAST_REPORT/depedancey-scan --prettyPrint
+dependency-check --scan `pwd` -f JSON -o $SAST_REPORT/depedancey-scan --prettyPrint
